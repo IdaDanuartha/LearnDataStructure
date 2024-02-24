a6dfebe16aba5beeac7f0c945b305a611dfbe795
IdaDanuartha <danuart21@gmail.com>
2024-02-24 19:23:53 +0800

    add mini project for struct

---
+++ Pointer/01-MiniProject1/main.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//using namespace std;
+//
+//void multiply(int *score) {
+//    *score = *score * 5;
+////    cout << "SCORE UPDATED TO " << *score << endl;
+//}
+//
+//int main() {
+//    int score = 1;
+//
+//    cout << "Score sebelum diubah : " << score << endl;
+//    multiply(&score);
+//    multiply(&score);
+//    multiply(&score);
+//    multiply(&score);
+//    multiply(&score);
+//    cout << "Score setelah diubah : " << score;
+//}
