13188b75c664387e108e20bd72ed94bf7bb182d7
IdaDanuartha <danuart21@gmail.com>
2024-03-01 01:17:03 +0800

    add array project

---
+++ 01-Array/01-MiniProject1/main.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//#include<array>
+//using namespace std;
+//
+//int main() {
+//    int sum = 0;
+//    float avg;
+//
+//    array<int, 5> numbers;
+//    int size = numbers.size();
+//
+//    for(int i = 0; i < size; i++) {
+//        cout << "Angka " << i+1 << " : ";
+//        cin >> numbers[i];
+//    }
+//
+//    for(int i = 0; i < size; i++) {
+//        sum += numbers[i];
+//    }
+//
+//    avg = sum / size;
+//
+//    cout << "Rata-rata lima bilangan = " << avg;
+//}
